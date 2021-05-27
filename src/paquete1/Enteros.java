1b640c9eb021e69704ba1ba49c5cc181e50ce767
Pablo Lopez Roda <pablolr45@gmail.com>
2021-05-27 09:36:09 +0200

    commit 1

---
+++ src/paquete1/Enteros.java
@@ -0,0 +1,192 @@
+package paquete1;
+
+/**
+ * @Author Pablo Lopez Roda
+ *
+ */
+public class Enteros {
+    /**
+     *
+     * @param n
+     * @return
+     */
+    public static int r_factorial(int n) {
+        int resultado;
+
+        if (n == 0) {
+            resultado = 1;
+        } else {
+            resultado = n * r_factorial(n - 1);
+        }
+        return resultado;
+    }
+
+    /**
+     *
+     * @param n
+     * @return
+     */
+    public static int factorial(int n) {
+        int resultado;
+
+        resultado = 1;
+        for (int i = 2; i <= n; i++) {
+            resultado = resultado * i;
+        }
+        return resultado;
+    }
+
+    /**
+     *
+     * @param cadena
+     * @return
+     */
+    public static String alreves(String cadena) {
+        String resultado = "";
+
+        for (int i = cadena.length() - 1; i >= 0; i--) {
+            resultado = resultado + cadena.charAt(i);
+        }
+        return resultado;
+    }
+
+    /**
+     *
+     * @param n
+     * @return
+     */
+    public static boolean capicua(int n) {
+        boolean resultado;
+        String cadena;
+
+        cadena = Integer.toString(n);
+        if (cadena.equals(alreves(cadena))) {
+            resultado = true;
+        } else {
+            resultado = false;
+        }
+
+        return resultado;
+    }
+
+    /**
+     *
+     * @param multiplo
+     * @param divisor
+     * @return
+     */
+    public static boolean divisible(int multiplo, int divisor) {
+        boolean resultado;
+
+        if (multiplo % divisor == 0) {
+            resultado = true;
+        } else {
+            resultado = false;
+        }
+
+        return resultado;
+    }
+
+    /**
+     *
+     * @param mult
+     * @param divisor
+     * @return
+     */
+    public static boolean multiplo(int mult, int divisor) {
+        boolean resultado;
+
+        try {
+            if (mult % divisor == 0) {
+                resultado = true;
+            } else {
+                resultado = false;
+            }
+        } catch (ArithmeticException e) {
+            resultado = false;
+        }
+        return resultado;
+    }
+
+    /**
+     *
+     * @param n
+     * @return
+     */
+    public static boolean esPrimo(int n) {
+        boolean primo;
+
+        if (n <= 1) {
+            primo = false;
+        } else {
+            primo = true;
+            int i = 2;
+            while (primo && i <= n / 2) {
+                if (divisible(n, i)) {
+                    primo = false;
+                } else {
+                    i++;
+                }
+            }
+        }
+        return primo;
+    }
+
+    /**
+     *
+     * @param n
+     * @return
+     */
+    public static int n_primos(int n) {
+        int resultado = 0;
+
+        for (int i = 2; i <= n; i++) {
+            if (esPrimo(i)) {
+                resultado++;
+            }
+        }
+        return resultado;
+    }
+
+    /**
+     *
+     * @param n
+     * @return
+     */
+    public static int[] primosHasta(int n) {
+        int[] resultado;
+
+        resultado = new int[n_primos(n)];
+        int cont = 0;
+        for (int i = 2; i <= n; i++) {
+            if (esPrimo(i)) {
+                resultado[cont] = i;
+                cont++;
+            }
+        }
+
+        return resultado;
+    }
+
+    /**
+     *
+     * @param n
+     * @return
+     */
+    public static int[] primerosPrimos(int n) {
+        int[] resultado = new int[(n < 0) ? 0 : n];
+
+        int cont = 0;
+        int i = 2;
+        while (cont < n) {
+            if (esPrimo(i)) {
+                resultado[cont] = i;
+                cont++;
+            }
+            i++;
+        }
+
+        return resultado;
+    }
+}
+
